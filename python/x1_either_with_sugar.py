5ffa7b409213a25dacaa1ea0f6c5c06d91cb6ba4
Micah Shlain <mcshlain@gmail.com>
2025-08-13 13:05:57 +0300

    some progress with io

---
+++ python/x1_either_with_sugar.py
@@ -0,0 +1,209 @@
+from __future__ import annotations
+
+from collections.abc import Generator
+from dataclasses import dataclass
+from types import TracebackType
+from typing import Any, Callable, Never
+
+# -------------- #
+# Syntax as Data #
+# -------------- #
+
+
+@dataclass(frozen=True, slots=True)
+class StopFromError[E]:
+    error: E
+
+
+# --------------- #
+# The Either Type #
+# --------------- #
+
+type EitherYield[E] = StopFromError[E]
+type EitherSend = Any
+type EitherGen[E, A] = Generator[EitherYield[E], EitherSend, A]
+
+
+@dataclass(frozen=True, slots=True)
+class Either[E, A]:
+    decorated: EitherGen[E, A]
+
+    # generator protocol
+    def send(self, value: EitherSend) -> EitherYield[E]:
+        return self.decorated.send(value)
+
+    def throw(
+        self, typ: type[BaseException] | BaseException, val: Any | None = None, tb: TracebackType | None = None
+    ) -> EitherYield[E]:
+        return self.decorated.throw(typ, val, tb)
+
+    def close(self) -> None:
+        return self.decorated.close()
+
+    # iterator protocol
+    def __next__(self) -> EitherYield[E]:
+        return self.decorated.__next__()
+
+    def __iter__(self) -> EitherGen[E, A]:
+        return self
+
+    # basic operations
+    @staticmethod
+    def error[E2](error: E2) -> Either[E2, Never]:
+        return _error(error)
+
+    @staticmethod
+    def pure[A2](value: A2) -> Either[Never, A2]:
+        return _pure(value)
+
+    # Combinators
+    def recover_with[B](self, f: Callable[[E], B]) -> Either[Never, A | B]:
+        return Either(_RecoverWith(f, self))
+
+
+def eitherable[**P, E, A](
+    f: Callable[P, EitherGen[E, A]],
+) -> Callable[P, Either[E, A]]:
+    def _eitherable_internal(*args: P.args, **kwargs: P.kwargs) -> Either[E, A]:
+        return Either(f(*args, **kwargs))
+
+    return _eitherable_internal
+
+
+# ----------------------------------- #
+# Basis operations wrappers (private) #
+# ----------------------------------- #
+
+
+@eitherable
+def _error[E](error: E) -> EitherGen[E, Never]:
+    yield StopFromError(error)
+    # NOTE: unreachable raise (based on interpreter impl, needed to avoid interpreter thinking we return a None)
+    raise
+
+
+@eitherable
+def _pure[A](value: A) -> EitherGen[Never, A]:
+    return value
+    # NOTE: unreachable yield is how to force the interpreter to make this function a genertor
+    yield
+
+
+# ----------- #
+# Combinators #
+# ----------- #
+
+
+@dataclass(frozen=True)
+class _RecoverWith[E, A, B]:
+    recover: Callable[[E], B]
+    decorated: EitherGen[E, A]
+
+    # generator protocol
+    def send(self, value: EitherSend) -> EitherYield[Never]:
+        v = self.decorated.send(value)
+        if isinstance(v, StopFromError):
+            e = StopIteration()
+            e.value = self.recover(v.error)
+            raise e
+        return v
+
+    def throw(
+        self,
+        typ: type[BaseException] | BaseException,
+        val: Any | None = None,
+        tb: TracebackType | None = None,
+    ) -> Any:
+        return self.decorated.throw(typ, val, tb)
+
+    def close(self) -> None:
+        self.decorated.close()
+
+    # iterator protocol
+    def __next__(self) -> EitherYield[Never]:
+        v = self.decorated.__next__()
+        if isinstance(v, StopFromError):
+            e = StopIteration()
+            e.value = self.recover(v.error)
+            raise e
+        return v
+
+    def __iter__(self) -> EitherGen[Never, A | B]:
+        return self
+
+
+# ----------- #
+# Interpreter #
+# ----------- #
+
+
+def run_either[E, A](exp: Either[E, A]) -> A | StopFromError[E]:
+    try:
+        next_send: Any = None
+        while True:
+            command = exp.send(next_send)
+            match command:
+                case StopFromError() as stp:
+                    exp.close()
+                    return stp
+    except StopIteration as e:
+        return e.value
+
+
+# ------- #
+# Example #
+# ------- #
+
+
+@eitherable
+def divide(a: int, b: int) -> EitherGen[str, int]:
+    if b == 0:
+        x = yield from Either.error(f"can't divide {a} by 0")
+        return x
+    else:
+        return int(a / b)
+
+
+@eitherable
+def prog1() -> EitherGen[str, int]:
+    a = yield from Either.pure(6)
+    b = yield from Either.pure(3)
+    r = yield from divide(a, b)
+    return r
+
+
+@eitherable
+def prog2() -> EitherGen[str, int]:
+    a = yield from Either.pure(6)
+    b = yield from Either.error("prog2 mb intentional error")
+    r = yield from divide(a, b)
+    return r
+
+
+@eitherable
+def prog3() -> EitherGen[str, int]:
+    a = yield from Either.pure(6)
+    b = yield from Either.pure(0)
+    r = yield from divide(a, b)
+    return r
+
+
+@eitherable
+def prog4() -> EitherGen[Never, int | str]:
+    r = yield from divide(6, 0).recover_with(lambda _: "oops")
+    return r
+
+
+def main() -> None:
+    r1 = run_either(prog1())
+    r2 = run_either(prog2())
+    r3 = run_either(prog3())
+
+    print(f"{r1=}\n{r2=}\n{r3=}")
+
+    r4 = run_either(prog4())
+    print(f"{r4=}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/z1_io_basic_async.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Generator
+from dataclasses import dataclass
+from types import TracebackType
+from typing import Any, Callable, Never, Sequence
+
+
+@dataclass(frozen=True, slots=True)
+class SubTask[E, A]:
+    suspended_io: IO[E, A]
+
+
+# -------------- #
+# Syntax as Data #
+# -------------- #
+
+
+@dataclass(frozen=True, slots=True)
+class StopFromError[E]:
+    error: E
+
+
+@dataclass(frozen=True, slots=True)
+class Join:
+    subtasks: Sequence[SubTask[Any, Any]]
+
+
+# ---------------- #
+# They Either Type #
+# ---------------- #
+
+type IOYield[E] = StopFromError[E] | Join
+type IOSend = Any
+type IO[E, A] = Generator[IOYield[E], IOSend, A]
+
+# ------------------------- #
+# Basis operations wrappers #
+# ------------------------- #
+
+
+def halt_with_error[E](error: E) -> IO[E, Never]:
+    yield StopFromError(error)
+    # NOTE: unreachable raise (based on interpreter impl, needed to avoid interpreter thinking we return a None)
+    raise
+
+
+def pure[A](value: A) -> IO[Never, A]:
+    return value
+    # NOTE: unreachable yield is how to force the interpreter to make this function a genertor
+    yield
+
+
+def fork[E, A](comp: IO[E, A]) -> SubTask[E, A]:
+    return SubTask(comp)
+
+
+# NOTE: Need many overloads to get type safe return value
+def join[E](sub_tasks: Sequence[SubTask[E, Any]]) -> IO[E, Sequence[Any]]:
+    r = yield Join(sub_tasks)
+    return r
+
+
+# ----------- #
+# Combinators #
+# ----------- #
+
+
+@dataclass(frozen=True)
+class _RecoverWith[E, A, B]:
+    decorated: IO[E, A]
+    recover: Callable[[E], B]
+
+    # generator protocol
+    def send(self, value: IOSend) -> IOYield[Never]:
+        v = self.decorated.send(value)
+        if isinstance(v, StopFromError):
+            e = StopIteration()
+            e.value = self.recover(v.error)
+            raise e
+        return v
+
+    def throw(
+        self,
+        typ: type[BaseException] | BaseException,
+        val: Any | None = None,
+        tb: TracebackType | None = None,
+    ) -> Any:
+        return self.decorated.throw(typ, val, tb)
+
+    def close(self) -> None:
+        self.decorated.close()
+
+    # iterator protocol
+    def __next__(self) -> IOYield[Never]:
+        v = self.decorated.__next__()
+        if isinstance(v, StopFromError):
+            e = StopIteration()
+            e.value = self.recover(v.error)
+            raise e
+        return v
+
+    def __iter__(self) -> IO[Never, A | B]:
+        return self
+
+
+def recover_with[E, A, B](either: IO[E, A], recover: Callable[[E], B]) -> IO[Never, A | B]:
+    return _RecoverWith(either, recover)
+
+
+# ----------- #
+# Interpreter #
+# ----------- #
+
+
+async def run_io[E, A](exp: IO[E, A]) -> A | StopFromError[E]:
+    try:
+        next_send: Any = None
+        while True:
+            command = exp.send(next_send)
+            step_result = await run_single_command(command)
+            if isinstance(step_result, StopFromError):
+                exp.close()
+                return step_result
+
+            next_send = step_result
+    except StopIteration as e:
+        return e.value
+
+
+async def run_single_command[E](command: IOYield[E]) -> Any | StopFromError[E]:
+    match command:
+        case StopFromError() as stp:
+            return stp
+        case Join(sub_commands):
+            sub_results = await asyncio.gather(*[run_io(st.suspended_io) for st in sub_commands])
+            for sr in sub_results:
+                if isinstance(sr, StopFromError):
+                    return sr  # NOTE: arbitrarily return the first error, can also do something else
+            return sub_results
+
+
+# ------- #
+# Example #
+# ------- #
+
+
+@dataclass(frozen=True, slots=True)
+class PrefixToLong:
+    pass
+
+
+def prefix_of(s: str, /, length: int) -> IO[PrefixToLong, str]:
+    if len(s) < length:
+        x = yield from halt_with_error(PrefixToLong())
+        return x
+    else:
+        return s[0:length]
+
+
+@dataclass(frozen=True, slots=True)
+class SuffixToLong:
+    pass
+
+
+def suffix_of(s: str, /, length: int) -> IO[SuffixToLong, str]:
+    if len(s) < length:
+        # x = yield from halt_with_error(f"can't take the suffix of '{s}' of lengh {length}, it's not long enough")
+        x = yield from halt_with_error(SuffixToLong())
+        return x
+    else:
+        return s[len(s) - length :]
+
+
+def prog1() -> IO[PrefixToLong | SuffixToLong, int]:
+    st1 = fork(prefix_of("abcdefgh", 4))
+    st2 = fork(suffix_of("abcdefgh", 2))
+
+    r1, r2 = yield from join([st1, st2])
+    return r1 + r2
+
+
+def prog2() -> IO[PrefixToLong | SuffixToLong, int]:
+    st1 = fork(prefix_of("abcdefgh", 4))
+    st2 = fork(suffix_of("abcdefgh", 20))
+
+    r1, r2 = yield from join([st1, st2])
+    return r1 + r2
+
+
+async def main() -> None:
+    r1 = await run_io(prog1())
+
+    print(f"{r1=}")
+
+    r2 = await run_io(prog2())
+
+    print(f"{r2=}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
